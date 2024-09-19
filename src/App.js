4c9797932afc19a5dab93bde78951aa16a367db4
Alianne Dosal Allison <adosalallison@gmail.com>
2024-09-19 10:27:15 +0200

    Added Boostrap

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
 import Weather from "./Weather";
 import "./App.css";
+import "bootstrap/dist/css/bootstrap.css";
 
 function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <Weather />
-      <footer>
-        This project was coded by{" "}
-        <a href="https://i-alianne.com/" target="_blank" rel="noreferrer">
-          Alianne
-        </a>{" "}
-        and is open-sourced on{" "}
-        <a
-          href="https://github.com/i-alianne/react-app"
-          target="_blank"
-          rel="noreferrer"
-        >
-          GitHub
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <Weather />
+        <footer>
+          This project was coded by{" "}
+          <a href="https://i-alianne.com/" target="_blank" rel="noreferrer">
+            Alianne
+          </a>{" "}
+          and is open-sourced on{" "}
+          <a
+            href="https://github.com/i-alianne/react-app"
+            target="_blank"
+            rel="noreferrer"
+          >
+            GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
