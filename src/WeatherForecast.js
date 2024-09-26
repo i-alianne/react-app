6b1fbdd8a150d0e4a3f4ad665d1886e7fd47a94a
Alianne Dosal Allison <adosalallison@gmail.com>
2024-09-26 11:49:22 +0200

    API call

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,7 +1,17 @@
 import React from "react";
 import "./WeatherForecast.css";
+import axios from "axios";
 
 export default function WeatherForecast(props) {
+  function handleResponse(response) {
+    console.log(response.data);
+  }
+
+  const apiKey = "be427b594372a38b6f2t7o035dc1e24d";
+  let city = props.city;
+  let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(handleResponse);
+
   return (
     <div className="WeatherForecast">
       <div className="row">
